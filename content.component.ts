5763dbf348366e89a0e283f608362da3ebd0aa02
Anna <105435188+ANAOLLYB@users.noreply.github.com>
2024-02-23 16:52:28 -0300

    Add files via upload

---
+++ content.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { dataFake } from 'src/app/data/dataFake';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  photoCover: string=""
+  contentTitle: string=""
+  contentDescription: string=""
+
+
+
+constructor(
+
+){}
+
+  }
+
+
+
+
---
+++ dataFake.ts
@@ -0,0 +1,11 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo": "aaaa",
+    "title": "Novo Filme",
+    "descriotion": "EI",
+  }
+
+
+
+]
